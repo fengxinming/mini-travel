41320c2e7df26f76c91ced3f7feabc006ca9ed91
fengxinming <fxm0016@126.com>
2018-03-26 22:05:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# mini-travel
+
+> Note: that `mini-travel` supports being visited on WeChat
+
+<br/>
+
+# Development & Deployment
+
+## How to get codes from remote GIT repository
+
+```bash
+$ git@gitlab.ziztour.loc:tmc/mini-travel.git
+$ cd mini-travel
+$ cnpm install
+```
+
+## How to start development in local environment
+
+```bash
+$ wepy build --watch
+```
+
+## How to pack JS components
+
+```bash
+$ wepy build
+```
---
+++ src/pages/index.vue
@@ -0,0 +1,69 @@
+
+<template lang='pug'>
+  view.container
+  view.userinfo(@tap='handleViewTap')
+    image.userinfo-avatar(src='{{ userInfo.avatarUrl }}', background-size='cover')
+      view.userinfo-nickname {{ userInfo.nickName }}
+  panel
+    | hello world
+
+</template>
+
+<script>
+import wepy from 'wepy';
+import Panel from '@/components/panel';
+import testMixin from '../mixins/test';
+
+export default class Index extends wepy.page {
+  config = {
+    navigationBarTitleText: 'test'
+  };
+  components = {
+    panel: Panel
+  };
+
+  mixins = [testMixin];
+
+  data = {
+    userInfo: {
+      nickName: '加载中...'
+    }
+  };
+
+  computed = {};
+
+  methods = {};
+
+  events = {};
+
+  onLoad() {
+    let self = this;
+    this.$parent.getUserInfo(function(userInfo) {
+      if (userInfo) {
+        // self.userInfo = userInfo;
+        // self.$apply();
+        self.setData({ userInfo: userInfo });
+      }
+    });
+  }
+}
+</script>
+
+<style lang="stylus">
+
+.userinfo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.userinfo-avatar {
+  width: 80rpx;
+  height: 80rpx;
+  border-radius: 50%;
+}
+
+.userinfo-nickname {
+  color: #aaa;
+}
+</style>
